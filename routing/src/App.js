605f4a8ee000fdc5f7866d93619bf1b9f0e4b494
Laurence2014 <kidulaura2014@yahoo.fr>
2023-01-23 13:04:17 +0200

    the about

---
+++ routing/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Nav from './Nav';
+import About from './About';
+import Shop from './Shop';
+
+function App() {
+  return (
+    <div className="App">
+     <Nav />
+     <About />
+     <Shop />
+    </div>
+  );
+}
+
+export default App;
